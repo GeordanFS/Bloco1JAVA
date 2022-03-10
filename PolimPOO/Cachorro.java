c24c5ef54c2f676f8ff849aa1dc128569c2ca9ad
unknown <geordansantos@gmail.com>
2022-03-10 17:30:57 -0300

    Exercicio de polimorfismo

---
+++ PolimPOO/Cachorro.java
@@ -0,0 +1,16 @@
+package PolimPOO;
+
+public class Cachorro extends Animal
+{
+	boolean correr;
+public void correndo()
+{
+	if(correr=true);
+	System.out.println("O cachorro está correndo");
+}
+@Override
+public void som() {
+	System.out.println("AUUUUUUUUU");
+	
+}
+}
---
+++ PolimPOO/Preguiça.java
@@ -0,0 +1,16 @@
+package PolimPOO;
+
+public class Preguiça extends Animal
+{
+	boolean subir;
+public void subindo()
+{
+	if(subir=true);
+	System.out.println("A preguiça está subindo na árvore");
+}
+@Override
+public void som() {
+	System.out.println("zzzzzzzzZZZZZZzzzzz");
+	
+}
+}
